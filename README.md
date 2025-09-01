ee1c18677e43d232ac3862b7f360fab31a7ca21d
giuliadoda <giuliadoda99@gmail.com>
2025-09-01 15:00:03 +0200

    data script added

---
+++ README.md
@@ -0,0 +1,13 @@
+Final project of *Neural Networks & Deep Learning* course.
+
+Based on [*Neural network time-series classifiers for gravitational-wave searches in single-detector periods* by Trovato *et al.*, 2023](https://arxiv.org/abs/2307.09268).
+
+The complete dataset can be found [here](https://zenodo.org/records/11093596).
+
+The aim of this project is to implement various time series classifiers and analyze their performances in order to establish the best model for signal - noise classification.
+
+Three neural networks model have been implemented:
+- Convolutional Neural Network,
+- [Temporal Convolutional Neural Network](https://arxiv.org/abs/1803.01271),
+- [Inception Time Neural Network](https://link.springer.com/article/10.1007/s10618-020-00710-y).
+They have been used to perform time series classification between data segments containing gravitational waves signal or noise. 
---
+++ create_dataset.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pickle
+
+data_path = "/Users/giuliadoda/university/PoD/NNDL/NNDL_final_proj/gw_data/" # CHANGE
+
+# dataset splitting percentages
+
+p_train = 0.6
+p_valid = 0.15
+p_test = 0.25
+
+seed = 0
+
+
+def load_concat(label, path=data_path):
+    """
+    Load files and create a dataset for each class.
+    """
+
+    f = np.load(path+'/'+label+'_train.npz')
+    X_train, Y_train = f['X'], f['Y']
+
+    f = np.load(path+'/'+label+'_test.npz')
+    X_test, Y_test = f['X'], f['Y']
+
+    X = np.concatenate((X_train, X_test), axis=0)
+    Y = np.concatenate((Y_train, Y_test), axis=0)
+
+    data = np.concatenate((X,Y), axis=1)
+
+def dataset_split(dataset, train=p_train, valid=p_valid, test=p_test):
+    """
+    Split each dataset into train, validation and test.
+    """
+    N = len(dataset)
+    n_train = int(p_train*N)
+    n_valid = int(p_valid*N)
+    n_test = int(p_test*N)
+
+    np.random.shuffle(dataset) # to ensure we have no bias
+
+    train = dataset[:n_train]
+    valid = dataset[n_train:n_train+n_valid]
+    test = dataset[-n_test:]
+
+    return train, valid, test
+
+
+# --- MAIN --- #
+
+if __name__=="__main__":
+
+    np.random.seed(seed)
+
+    # load data
+    noise_dataset = load_concat('noise')
+    signal_dataset = load_concat('signal')
+    glitch_dataset = load_concat('glitch')
+
+    # number of samples
+    N = len(noise_dataset)
+    S = len(signal_dataset)
+    G = len(glitch_dataset)
+    N_samples = N+S+G
+
+    print('Total samples: ', N_samples)
+
+    # split datasets
+    noise_train, noise_valid, noise_test = dataset_split(noise_dataset)
+    signal_train, signal_valid, signal_test = dataset_split(signal_dataset)
+    glitch_train, glitch_valid, glitch_test = dataset_split(glitch_dataset)
+
+    # merge by labels
+    training_dataset = np.concatenate((noise_train, signal_train, glitch_train))
+    validation_dataset = np.concatenate((noise_valid, signal_valid, glitch_valid))
+    test_dataset = np.concatenate((noise_test, signal_test, glitch_test))
+
+    # shuffle
+    np.random.shuffle(training_dataset)
+    np.random.shuffle(validation_dataset)
+    np.random.shuffle(test_dataset)
+
+    # save datasets
+    training_file = data_path+'training_data.pkl'
+    with open(training_file, 'wb') as file:
+        pickle.dump(training_dataset, file)
+
+    validation_file = data_path+'validation_data.pkl'
+    with open(validation_file, 'wb') as file:
+        pickle.dump(validation_dataset, file)
+
+    test_file = data_path+'test_data.pkl'
+    with open(test_file, 'wb') as file:
+        pickle.dump(test_dataset, file)
